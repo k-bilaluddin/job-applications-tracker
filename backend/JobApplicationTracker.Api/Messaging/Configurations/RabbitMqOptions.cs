8cab57702a888688d38cab95adeb968b6b3e4ae9
Khawaja Bilal Uddin <mbilaluddin1994@gmail.com>
2026-04-23 19:09:37 +0200

    publish JobApplicationStatusChanged event after status update (#33)

--- backend/JobApplicationTracker.Api/Messaging/Configurations/RabbitMqOptions.cs
+++ backend/JobApplicationTracker.Api/Messaging/Configurations/RabbitMqOptions.cs
@@ -6,5 +6,6 @@
         public string ConnectionString { get; set; } = string.Empty;
         public string JobApplicationCreatedQueueName { get; set; } = string.Empty;
         public string JobApplicationUpdatedQueueName { get; set; } = string.Empty;
+        public string JobApplicationStatusChangedQueueName { get; set; } = string.Empty;
     }
 }
